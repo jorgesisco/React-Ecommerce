14dbe4e7b4ed898c02c0b6f07822ec3645662765
Jorge Sisco <jorgesisco17@gmail.com>
2021-05-03 21:23:52 -0400

    Fetching Products From React (useEffect)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreens';
+import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
@@ -13,7 +13,7 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Route path='/' component={HomeScreen} exact />
-          <Route path='/Product/:id' component={ProductScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
 
