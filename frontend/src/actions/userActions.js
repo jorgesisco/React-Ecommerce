176dc4036ab996f2152063b214733ae84b44d99a
Jorge Sisco <jorgesisco17@gmail.com>
2021-05-19 12:25:03 -0400

    Update User Profile #3

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -110,7 +110,7 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
 
     const config = {
       headers: {
-        // 'Content-Type': 'aplication/json',
+        'Content-Type': 'application/json',
         Authorization: `Bearer ${userInfo.token}`,
       },
     };
